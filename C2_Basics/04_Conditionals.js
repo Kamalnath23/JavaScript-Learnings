054a165809dc1d728568c1a22ac1e373666fa4b6
Kamalnath <kamalnath121998@gmail.vom>
2023-12-29 13:05:22 +0530

    Conditionals - 1

---
+++ C2_Basics/04_Conditionals.js
@@ -0,0 +1,14 @@
+var temprature;
+//todo: go to google and get the data
+
+temprature =20;
+
+if(temprature<20){
+    console.log("Weather is cold outside.");
+}else if(temprature==20){
+    console.log("Weather is cold and enjoyable outside.");
+}else if(temprature >20 && temprature <30){
+    console.log("Weather is moderate outside.");
+}else{
+    console.log("Weather is hot outside.");
+}
