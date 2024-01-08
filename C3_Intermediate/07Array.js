6fe78adf7ede0129281ad03723c952d29120d418
Kamalnath <kamalnath121998@gmail.vom>
2024-01-08 13:53:17 +0530

    Arrays in Js

---
+++ C3_Intermediate/07Array.js
@@ -0,0 +1,36 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+var countries = ["India", "USA", "Russia", 'Germany","Australia', "Japan", "Thailand"];
+var states = new Array("Rajasthan", "Tamilnadu","Kerala","Karnataka","Delhi", "Himachal Pradesh","Punjab", "Mumbai", "Assam");
+
+//access values:
+console.log(states[0]);
+
+//properties
+console.log(states.length);
+
+//replace the value
+states[0] = "Jammu and Kashmir"
+console.log(states[0]);
+
+var user = ["Kamal","Sandhiya"]
+
+var userData = ["Kamal","Kamal@1234@co.test",3,34,true];
+console.log(userData);
+
+userData.pop();
+userData.pop();
+console.log(userData);
+
+userData.unshift("New value");
+console.log(userData);
+
+userData.shift();
+console.log(userData);
+
+console.log(userData.indexOf(3)); //search for anything specific.
+console.log(userData.indexOf("Kamal"));
+console.log(userData.indexOf("newbie"));
+
+console.log("Sandhiya");
+console.log(Array.from("Sandhiya"));
