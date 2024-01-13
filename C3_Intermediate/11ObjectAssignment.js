8301fc7cf979d3b42dabff86f194bd2071a589ec
Kamalnath <kamalnath121998@gmail.vom>
2024-01-13 13:11:21 +0530

    Objects in JS

---
+++ C3_Intermediate/11ObjectAssignment.js
@@ -0,0 +1,21 @@
+var OnePlus11 = {
+    DeviceName : "Oneplus 11 5G",
+    Storage: "256 GB",
+    Model : "CPH2447",
+    Processor: "Snapdragon 8 Gen 2 Mobile Platform",
+    BatteryCapacity_in_mAh : 5000,
+    ScreenSizeInCm : 17.02,
+    RAM: "16.0 GB",
+    Android:  true,
+    AndroidVersion:13,
+    OS: "OxygenOS",
+    OS_Version: 13.1,
+    x: 'x'
+};
+
+console.table(OnePlus11);
+
+console.log(OnePlus11.Android);
+console.log(OnePlus11.AndroidVersion);
+console.log(OnePlus11.OS);
+console.log(OnePlus11.OS_Version);
