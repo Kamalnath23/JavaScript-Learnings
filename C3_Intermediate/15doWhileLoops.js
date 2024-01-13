487be1e460e47ed0d229be68cb65e3a0436f161f
Kamalnath <kamalnath121998@gmail.vom>
2024-01-13 16:05:47 +0530

    Loops: For, while and do while basics

---
+++ C3_Intermediate/15doWhileLoops.js
@@ -0,0 +1,12 @@
+const myStates = ["Rajasthan", "Tamil Nadu" , "Kerala", "Karanataka", "Andhra pradesh",
+"Assam",  "Maharasthra", "Telangana", "J&K", "Himachal Pradhesh", "Madhya Pradesh",
+"Uttar Pradesh", "Punjab", "Gujarat", "Nagpur", "Bihar", "Chattisgarh", "Goa", "Jharkhand", 
+"Arunachal Pradesh", "Manipur", "West Bengal", "Mizoram", "Tripura", "Sikkim", "Odisha",
+"Nagaland", "Delhi", "Meghalaya", "Uttarakhand"
+]
+
+let index=0
+ do{
+    console.log(myStates[index]);
+    index++;
+}while (index < myStates.length)
